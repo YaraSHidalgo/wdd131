b98b75d1aac22937576fc3cf5d25679333da392c
@YaraSHidalgo <u382878978@byui.edu>
2024-11-09 23:40:30 -0300

    update

---
+++ scripts/getdates.js
@@ -0,0 +1,12 @@
+// Current year 
+const currentYear = new Date().getFullYear();
+const footerYear = document.querySelector('footer p:first-of-type'); 
+if (footerYear) {
+    footerYear.textContent = `© ${currentYear} Yara Hidalgo. Argentina.`;
+}
+// last modified date
+const lastModifiedDate = document.lastModified; //last modified date of the document
+const footerModified = document.querySelector('footer p:nth-of-type(2)'); 
+if (footerModified) {
+    footerModified.textContent = `Last modified: ${lastModifiedDate}`;
+}
