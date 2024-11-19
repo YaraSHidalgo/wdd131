fbcfceec4e65d26357cc24bcea6d8075ab817d61
@YaraSHidalgo <u382878978@byui.edu>
2024-11-18 23:27:57 -0300

    update temple activity

---
+++ week02-Assigment/scripts/temples.js
@@ -0,0 +1,12 @@
+//var to hold the current year
+const thisYear = new Date().getFullYear();
+//var to hold the las modify on the page
+const lastModify = document.lastModified;
+
+//span message with the current year
+document.querySelector('#currentYear').textContent = `© ${thisYear} Yara Hidalgo - Argentina`;
+// the last update of the page
+document.querySelector('#lastUpdate').textContent = `Last Update: ${lastModify}`;
+
+window.onload = updateInfo;
+
